560d1bce502537b182801133c593baa539882c8a
Preya Patel <147451844+preya-patel-125@users.noreply.github.com>
2026-06-04 15:16:19 -0400

    added simulation method

--- agent/train.py
+++ agent/train.py
@@ -1,8 +1,10 @@
 import random
+from time import sleep
 from environment import PokemonEnv
 from agent import QLearningAgent
+import sys
 
-def train(num_episodes=5000):
+def train(num_episodes=10000):
     env = PokemonEnv()
     agent = QLearningAgent(alpha=0.1, gamma=0.9, epsilon=1.0)
     decay_rate = 0.995
@@ -35,7 +37,34 @@ def train(num_episodes=5000):
             print(f"Episode {episode + 1}/{num_episodes} | Avg Reward (last 500): {avg_reward:.2f} | Epsilon: {agent.epsilon:.3f}")
     
     agent.save_q_table()
-    return agent, episode_rewards
+
+def simulation():
+    env = PokemonEnv()
+    state = env.reset()
+    agent = QLearningAgent(alpha = 0.1, gamma = 0.9, epsilon = 0)
+    agent.load_q_table()
+    done = False; 
+
+    print("Hi, I'm Brock! I'm Pewter's Gym Leader!\n"
+    "I believe in rock hard defense and determination!\n"
+    "That's why my Pokemon are all the rock-type!\n"
+    "Do you still want to challenger me?\n"
+    "Fine then! Show me your best!")
+
+    while not done:
+        possible_actions = env.get_possible_actions()
+        action = agent.get_action(state, possible_actions)
+
+        next_state, done, log = env.step(action)
+        
+        for i in log:
+            print(i)
+            sleep(0.5) 
+
+        state = next_state
 
 if __name__ == "__main__":
-    agent, rewards = train()
+    if "train" in sys.argv:
+        train()
+    else:
+        simulation()
